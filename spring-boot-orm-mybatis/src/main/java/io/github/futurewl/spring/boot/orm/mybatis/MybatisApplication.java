5bc2771c9cadefeb6ed21b31ecbb5fd934ca8d4a
魏来 <624263934@qq.com>
2018-12-07 22:25:42 +0800

    update

---
+++ spring-boot-orm-mybatis/src/main/java/io/github/futurewl/spring/boot/orm/mybatis/MybatisApplication.java
@@ -0,0 +1,17 @@
+package io.github.futurewl.spring.boot.orm.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述：MyBatis 应用
+ *
+ * @author weilai create by 2018-12-07:22:14
+ * @version 1.0
+ */
+@SpringBootApplication
+public class MybatisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+}
--- spring-boot-web/spring-boot-web-beetl/src/main/java/io/github/futurewl/sprng/boot/web/beetl/BeetlApplication.java
+++ spring-boot-web/spring-boot-web-beetl/src/main/java/io/github/futurewl/sprng/boot/web/beetl/BeetlApplication.java
@@ -3,6 +3,7 @@ package io.github.futurewl.sprng.boot.web.beetl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 功能描述：Beetl 应用启动
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author weilai create by 2018-12-07:12:40
  * @version 1.0
  */
+@EnableSwagger2
 @SpringBootApplication
 public class BeetlApplication {
 
