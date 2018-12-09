317477a896dee30d6a1962404fb050918f558640
魏来 <624263934@qq.com>
2018-12-09 22:37:11 +0800

    update

--- spring-boot-helloworld/src/main/java/io/github/futurewl/spring/boot/helloworld/HelloWorldApplication.java
+++ spring-boot-helloworld/src/main/java/io/github/futurewl/spring/boot/helloworld/HelloWorldApplication.java
@@ -1,7 +1,11 @@
 package io.github.futurewl.spring.boot.helloworld;
 
+import io.github.futurewl.spring.boot.helloworld.prop.UserProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +19,37 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class HelloWorldApplication {
 
+    @Value("${userinfo.name}")
+    private String username;
+
+    @Value("${userinfo.age}")
+    private Integer age;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private UserProperties userProperties;
+
     @RequestMapping("/")
     public String home() {
-        return "Hello World!";
+        return "Hello " + username + age + "!";
+    }
+
+    @RequestMapping("/a")
+    public String homea() {
+        return "Helloaaaaa "
+                + environment.getProperty("userinfo.name")
+                + environment.getProperty("userinfo.age")
+                + "!";
+    }
+
+    @RequestMapping("/b")
+    public String homeb() {
+        return "Hellobbb "
+                + userProperties.getName()
+                + userProperties.getAge()
+                + "!";
     }
 
     /**
