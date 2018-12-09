c3c2b175077c003e6999a298e85790f53bcbaca0
魏来 <624263934@qq.com>
2018-12-09 23:12:43 +0800

    update

---
+++ spring-boot-ms-elk/src/main/java/io/github/futurewl/spring/boot/ms/elk/ElkApplication.java
@@ -0,0 +1,10 @@
+package io.github.futurewl.spring.boot.ms.elk;
+
+/**
+ * 功能描述：ELK Spring Boot 启动类
+ *
+ * @author weilai create by 2018-12-09:22:48
+ * @version 1.0
+ */
+public class ElkApplication {
+}
