2bee9f2fab2ee7ef8e1eea483ab264f4a6e99cdb
魏来 <624263934@qq.com>
2018-12-09 22:12:53 +0800

    update

---
+++ spring-boot-cache-redis/src/main/java/io/github/futurewl/spring/boot/cache/redis/RedisApplication.java
@@ -0,0 +1,19 @@
+package io.github.futurewl.spring.boot.cache.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述：Redis 应用案例启动类
+ *
+ * @author weilai create by 2018-12-09:21:35
+ * @version 1.0
+ */
+@SpringBootApplication
+public class RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class, args);
+    }
+
+}
