9dc58b3962a977fc4248ce6a781dba62649d63da
魏来 <624263934@qq.com>
2019-02-20 15:09:23 +0800

    SpringBoot非官方教程 | 第六篇：springboot整合mybatis

---
+++ spring-boot-mybatis/src/main/java/io/github/futurewl/service/AccountService.java
@@ -0,0 +1,46 @@
+package io.github.futurewl.service;
+
+import io.github.futurewl.dao.AccountMapper;
+import io.github.futurewl.entity.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 功能描述：
+ *
+ * @author weilai create by 2019-02-20:14:27
+ * @version 1.0
+ */
+@Service
+public class AccountService {
+
+    private final AccountMapper accountMapper;
+
+    @Autowired
+    public AccountService(AccountMapper accountMapper) {
+        this.accountMapper = accountMapper;
+    }
+
+    public int add(String name, double money) {
+        return accountMapper.add(name, money);
+    }
+
+    public int update(String name, double money, int id) {
+        return accountMapper.update(name, money, id);
+    }
+
+    public int delete(int id) {
+        return accountMapper.delete(id);
+    }
+
+    public Account findAccount(int id) {
+        return accountMapper.findAccount(id);
+    }
+
+    public List<Account> findAccountList() {
+        return accountMapper.findAccountList();
+    }
+
+}
