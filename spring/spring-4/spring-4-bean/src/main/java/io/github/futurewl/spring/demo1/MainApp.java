bd74337d98b574b150f30357e2de87a20659444d
FutureWL <624263934@qq.com>
2019-06-07 16:44:31 +0800

    update

---
+++ spring/spring-4/spring-4-bean/src/main/java/io/github/futurewl/spring/demo1/MainApp.java
@@ -0,0 +1,29 @@
+package io.github.futurewl.spring.demo1;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * 功能描述：BeanFactory 容器
+ * <p>
+ * 这是一个最简单的容器，它主要的功能是为依赖注入 （DI） 提供支持，
+ * 这个容器接口在 org.springframework.beans.factory.BeanFactor 中被定义。
+ * BeanFactory 和相关的接口，比如BeanFactoryAware、DisposableBean、InitializingBean，仍旧保留在 Spring 中，
+ * 主要目的是向后兼容已经存在的和那些 Spring 整合在一起的第三方框架。
+ * <p>
+ * 在 Spring 中，有大量对 BeanFactory 接口的实现。其中，最常被使用的是 XmlBeanFactory 类。
+ * 这个容器从一个 XML 文件中读取配置元数据，由这些元数据来生成一个被配置化的系统或者应用。
+ * <p>
+ * 在资源宝贵的移动设备或者基于 applet 的应用当中， BeanFactory 会被优先选择。
+ * 否则，一般使用的是 ApplicationContext，除非你有更好的理由选择 BeanFactory。
+ *
+ * @author weilai create by 2019-06-07:16:34
+ * @version 1.0
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("Beans1.xml"));
+        HelloWorld obj = (HelloWorld) factory.getBean("helloWorld");
+        obj.getMessage();
+    }
+}
---
+++ spring/spring-4/spring-4-bean/src/main/java/io/github/futurewl/spring/demo4/HelloWorld.java
@@ -0,0 +1,19 @@
+package io.github.futurewl.spring.demo4;
+
+/**
+ * 功能描述：
+ *
+ * @author weilai create by 2019-06-07:16:36
+ * @version 1.0
+ */
+public class HelloWorld {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void getMessage() {
+        System.out.println("Your Message : " + message);
+    }
+}
