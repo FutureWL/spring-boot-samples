e67c1a25452d90fa960e429311d5f637987a4b5f
FutureWL <624263934@qq.com>
2019-06-07 17:31:03 +0800

    update

---
+++ spring/spring-4/spring-4-bean/src/main/java/io/github/futurewl/spring/aw/demo2/MainApp.java
@@ -0,0 +1,19 @@
+package io.github.futurewl.spring.aw.demo2;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 功能描述：
+ *
+ * @author weilai create by 2019-06-07:17:26
+ * @version 1.0
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("aw/AwBeans2.xml");
+        TextEditor te = (TextEditor) context.getBean("textEditor");
+        te.spellCheck();
+    }
+}
