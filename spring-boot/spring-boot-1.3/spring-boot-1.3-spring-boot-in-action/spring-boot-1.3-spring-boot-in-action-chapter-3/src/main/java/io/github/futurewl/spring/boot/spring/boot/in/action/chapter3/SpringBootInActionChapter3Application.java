6d8f17b234a43ee0136d5ffa130c67044bd7a25b
FutureWL <624263934@qq.com>
2019-06-08 16:30:30 +0800

    update

---
+++ spring-boot/spring-boot-1.3/spring-boot-1.3-spring-boot-in-action/spring-boot-1.3-spring-boot-in-action-chapter-3/src/main/java/io/github/futurewl/spring/boot/spring/boot/in/action/chapter3/SpringBootInActionChapter3Application.java
@@ -0,0 +1,17 @@
+package io.github.futurewl.spring.boot.spring.boot.in.action.chapter3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述：
+ *
+ * @author weilai create by 2019-06-08:15:46
+ * @version 1.0
+ */
+@SpringBootApplication
+public class SpringBootInActionChapter3Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootInActionChapter3Application.class, args);
+    }
+}
