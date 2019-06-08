e988276f0eff878f0becd1d71a421ab48a9954a0
FutureWL <624263934@qq.com>
2019-06-08 15:42:29 +0800

    update

---
+++ spring-boot/spring-boot-1.3/spring-boot-1.3-spring-boot-in-action/spring-boot-1.3-spring-boot-in-action-chapter-1/src/main/java/io/github/futurewl/spring/boot/spring/boot/in/action/chapter1/SpringBootInActionReadingListApplication.java
@@ -0,0 +1,17 @@
+package io.github.futurewl.spring.boot.spring.boot.in.action.chapter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述：
+ *
+ * @author weilai create by 2019-06-08:12:10
+ * @version 1.0
+ */
+@SpringBootApplication
+public class SpringBootInActionReadingListApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootInActionReadingListApplication.class, args);
+    }
+}
---
+++ spring-boot/spring-boot-1.3/spring-boot-1.3-spring-boot-in-action/spring-boot-1.3-spring-boot-in-action-chapter-1/src/test/java/io/github/futurewl/spring/boot/spring/boot/in/action/chapter1/SpringBootInActionReadingListApplicationTest.java
@@ -0,0 +1,27 @@
+package io.github.futurewl.spring.boot.spring.boot.in.action.chapter1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * 功能描述：
+ *
+ * @author weilai create by 2019-06-08:12:13
+ * @version 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(
+        classes = SpringBootInActionReadingListApplication.class
+)
+@WebAppConfiguration
+public class SpringBootInActionReadingListApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
