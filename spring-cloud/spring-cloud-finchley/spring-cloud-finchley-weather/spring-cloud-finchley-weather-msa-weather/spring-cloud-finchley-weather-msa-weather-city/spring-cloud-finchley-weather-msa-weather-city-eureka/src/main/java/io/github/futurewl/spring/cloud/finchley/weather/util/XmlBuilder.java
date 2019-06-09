9c97c9a399235e63cda95cfc31795b11d54221bc
FutureWL <624263934@qq.com>
2019-06-09 14:46:23 +0800

    新增

---
+++ spring-cloud/spring-cloud-finchley/spring-cloud-finchley-weather/spring-cloud-finchley-weather-msa-weather/spring-cloud-finchley-weather-msa-weather-city/spring-cloud-finchley-weather-msa-weather-city-eureka/src/main/java/io/github/futurewl/spring/cloud/finchley/weather/util/XmlBuilder.java
@@ -0,0 +1,31 @@
+package io.github.futurewl.spring.cloud.finchley.weather.util;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+/**
+ * 功能描述：
+ *
+ * @author weilai create by 2019-06-08:21:54
+ * @version 1.0
+ */
+public class XmlBuilder {
+
+    public static Object xmlStrToObject(Class<?> clazz, String xmlStr) throws JAXBException, IOException {
+        Object xmlObject = null;
+        Reader reader = null;
+
+        JAXBContext context = JAXBContext.newInstance(clazz);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        reader = new StringReader(xmlStr);
+        xmlObject = unmarshaller.unmarshal(reader);
+        if (null != reader) {
+            reader.close();
+        }
+        return xmlObject;
+    }
+}
---
+++ spring-cloud/spring-cloud-finchley/spring-cloud-finchley-weather/spring-cloud-finchley-weather-msa-weather/spring-cloud-finchley-weather-msa-weather-collection/spring-cloud-finchley-weather-msa-weather-collection-eureka/src/main/java/io/github/futurewl/spring/cloud/finchley/weather/job/WeatherDataSyncJob.java
@@ -0,0 +1,53 @@
+package io.github.futurewl.spring.cloud.finchley.weather.job;
+
+import io.github.futurewl.spring.cloud.finchley.weather.service.WeatherDataCollectionService;
+import io.github.futurewl.spring.cloud.finchley.weather.vo.City;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 功能描述：天气数据同步任务
+ *
+ * @author weilai create by 2019-06-08:21:42
+ * @version 1.0
+ */
+@Component
+public class WeatherDataSyncJob extends QuartzJobBean {
+
+    private final static Logger logger = LoggerFactory.getLogger(WeatherDataSyncJob.class);
+
+    @Autowired
+    private WeatherDataCollectionService weatherDataCollectionService;
+
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        logger.info("Start 天气数据同步任务");
+
+        // todo 改为由城市数据 API 微服务来提供数据
+
+        List<City> cityList = null;
+        try {
+            cityList = new ArrayList<>();
+            City city = new City();
+            city.setCityId("101280601");
+            cityList.add(city);
+        } catch (Exception e) {
+            logger.error("获取城市信息异常！", e);
+            throw new RuntimeException("获取城市信息异常！", e);
+        }
+        for (City city : cityList) {
+            String cityId = city.getCityId();
+            logger.info("天气数据同步中,cityId:" + cityId);
+            weatherDataCollectionService.syncDataByCityId(cityId);
+        }
+        logger.info("End 天气数据同步任务");
+    }
+}
