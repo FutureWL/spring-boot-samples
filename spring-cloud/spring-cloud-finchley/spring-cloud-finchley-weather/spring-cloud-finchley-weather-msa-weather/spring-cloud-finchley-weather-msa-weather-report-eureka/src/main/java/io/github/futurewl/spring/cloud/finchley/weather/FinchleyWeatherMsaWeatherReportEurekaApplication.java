a239cf2f4e4f6958c124badea212e9ba867192fb
FutureWL <624263934@qq.com>
2019-06-09 14:21:58 +0800

    新增

---
+++ spring-cloud/spring-cloud-finchley/spring-cloud-finchley-weather/spring-cloud-finchley-weather-msa-weather/spring-cloud-finchley-weather-msa-weather-report-eureka/src/main/java/io/github/futurewl/spring/cloud/finchley/weather/FinchleyWeatherMsaWeatherReportEurekaApplication.java
@@ -0,0 +1,17 @@
+package io.github.futurewl.spring.cloud.finchley.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述：
+ *
+ * @author weilai create by 2019-06-09:13:46
+ * @version 1.0
+ */
+@SpringBootApplication
+public class FinchleyWeatherMsaWeatherReportEurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FinchleyWeatherMsaWeatherReportEurekaApplication.class, args);
+    }
+}
