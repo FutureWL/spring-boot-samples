00b045b2769af607de837742b231301e705e7c39
FutureWL <624263934@qq.com>
2019-06-16 13:23:34 +0800

    update

---
+++ spring-boot/spring-boot-2.0/spring-boot-2.0-flowable/src/main/java/io/github/futurewl/spring/boot/SpringBoot20FlowableApplication.java
@@ -0,0 +1,14 @@
+package io.github.futurewl.spring.boot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述：
+ *
+ * @author weilai create by 2019-06-14:14:15
+ * @version 1.0
+ */
+@SpringBootApplication
+public class SpringBoot20FlowableApplication {
+
+}
---
+++ spring-boot/spring-boot-2.0/spring-boot-2.0-web/src/main/java/io/github/futurewl/spring/boot/SpringBoot20WebApplication.java
@@ -0,0 +1,17 @@
+package io.github.futurewl.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述：
+ *
+ * @author weilai create by 2019-06-13:18:46
+ * @version 1.0
+ */
+@SpringBootApplication
+public class SpringBoot20WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot20WebApplication.class, args);
+    }
+}
