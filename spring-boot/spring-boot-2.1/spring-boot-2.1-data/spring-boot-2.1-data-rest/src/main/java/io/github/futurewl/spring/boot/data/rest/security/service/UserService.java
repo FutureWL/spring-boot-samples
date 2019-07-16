02364b051836abbe09301cf02408b3d3ea04f1c0
FutureWL <624263934@qq.com>
2019-07-16 14:09:24 +0800

    update

--- spring-boot/spring-boot-2.1/spring-boot-2.1-data/spring-boot-2.1-data-rest/src/main/java/io/github/futurewl/spring/boot/data/rest/security/service/UserService.java
+++ spring-boot/spring-boot-2.1/spring-boot-2.1-data/spring-boot-2.1-data-rest/src/main/java/io/github/futurewl/spring/boot/data/rest/security/service/UserService.java
@@ -8,5 +8,26 @@ import io.github.futurewl.spring.boot.data.rest.security.entity.User;
  * @author weilai
  */
 public interface UserService {
+
     User findById(Long id);
+
+    void handleUserCreateBefore(User user);
+
+    void handleUserCreateAfter(User user);
+
+    void handleUserSaveBefore(User user);
+
+    void handleUserSaveAfter(User user);
+
+    void handleUserDeleteBefore(User user);
+
+    void handleUserDeleteAfter(User user);
+
+    void handleUserLinkSaveBefore(User user);
+
+    void handleUserLinkSaveAfter(User user);
+
+    void handleUserLinkDeleteBefore(User user);
+
+    void handleUserLinkDeleteAfter(User user);
 }
--- spring-boot/spring-boot-2.1/spring-boot-2.1-data/spring-boot-2.1-data-rest/src/main/java/io/github/futurewl/spring/boot/data/rest/security/service/impl/UserServiceImpl.java
+++ spring-boot/spring-boot-2.1/spring-boot-2.1-data/spring-boot-2.1-data-rest/src/main/java/io/github/futurewl/spring/boot/data/rest/security/service/impl/UserServiceImpl.java
@@ -21,4 +21,54 @@ public class UserServiceImpl implements UserService {
     public User findById(Long id) {
         return userRepository.findById(id).get();
     }
+
+    @Override
+    public void handleUserCreateBefore(User user) {
+        System.out.println("处理创建用户之前");
+    }
+
+    @Override
+    public void handleUserCreateAfter(User user) {
+        System.out.println("处理创建用户之后");
+    }
+
+    @Override
+    public void handleUserSaveBefore(User user) {
+        System.out.println("处理保存用户之前");
+    }
+
+    @Override
+    public void handleUserSaveAfter(User user) {
+        System.out.println("处理保存用户之后");
+    }
+
+    @Override
+    public void handleUserDeleteBefore(User user) {
+        System.out.println("处理删除用户之前");
+    }
+
+    @Override
+    public void handleUserDeleteAfter(User user) {
+        System.out.println("处理删除用户之后");
+    }
+
+    @Override
+    public void handleUserLinkSaveBefore(User user) {
+        System.out.println("处理LinkSave用户之前");
+    }
+
+    @Override
+    public void handleUserLinkSaveAfter(User user) {
+        System.out.println("处理LinkSave用户之后");
+    }
+
+    @Override
+    public void handleUserLinkDeleteBefore(User user) {
+        System.out.println("处理LinkDelete用户之前");
+    }
+
+    @Override
+    public void handleUserLinkDeleteAfter(User user) {
+        System.out.println("处理LinkDelete用户之后");
+    }
 }
