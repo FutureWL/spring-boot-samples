c23b8b1468910065f77863cda4bf2e3b49c008b8
FutureWL <624263934@qq.com>
2019-08-05 18:15:53 +0800

    update

---
+++ spring-boot/spring-boot-2.1/spring-boot-2.1-security/spring-boot-2.1-security-rbac-application/src/main/java/io/github/futurewl/entity/User.java
@@ -0,0 +1,14 @@
+package io.github.futurewl.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 功能描述：
+ *
+ * @author weilai
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class User extends BaseEntity {
+}
