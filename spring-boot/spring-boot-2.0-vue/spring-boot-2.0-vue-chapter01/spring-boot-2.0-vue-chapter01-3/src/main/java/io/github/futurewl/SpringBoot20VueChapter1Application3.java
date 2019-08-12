b3e00f39cdf7b7ead5b4cf155f1da8ebd54fddf6
FutureWL <624263934@qq.com>
2019-08-12 09:13:10 +0800

    更新：

--- spring-boot/spring-boot-2.0-vue/spring-boot-2.0-vue-chapter01/spring-boot-2.0-vue-chapter01-3/src/main/java/io/github/futurewl/SpringBoot20VueChapter1Application3.java
+++ spring-boot/spring-boot-2.0-vue/spring-boot-2.0-vue-chapter01/spring-boot-2.0-vue-chapter01-3/src/main/java/io/github/futurewl/SpringBoot20VueChapter1Application3.java
@@ -1,5 +1,6 @@
-package org.sang;
+package io.github.futurewl;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,16 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Chapter013Application {
+public class SpringBoot20VueChapter1Application3 {
+
     @GetMapping("/hello")
     public String hello() {
         return "hello jetty";
     }
 
     public static void main(String[] args) {
-        SpringApplicationBuilder builder = new SpringApplicationBuilder(Chapter013Application.class);
-        builder.application().setAdditionalProfiles("dev");
-        builder.run(args);
-//        SpringApplication.run(Chapter013Application.class, args);
+//        SpringApplicationBuilder builder = new SpringApplicationBuilder(SpringBoot20VueChapter1Application3.class);
+//        builder.application().setAdditionalProfiles("dev");
+//        builder.run(args);
+        SpringApplication.run(SpringBoot20VueChapter1Application3.class, args);
     }
 }
