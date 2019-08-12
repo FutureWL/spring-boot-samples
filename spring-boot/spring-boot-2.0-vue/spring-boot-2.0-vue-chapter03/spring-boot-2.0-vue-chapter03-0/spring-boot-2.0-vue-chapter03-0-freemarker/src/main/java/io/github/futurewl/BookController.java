d0ffae2cd5e082490308270f014ac03d7804747c
FutureWL <624263934@qq.com>
2019-08-12 10:16:06 +0800

    更新：

--- spring-boot/spring-boot-2.0-vue/spring-boot-2.0-vue-chapter03/spring-boot-2.0-vue-chapter03-0/spring-boot-2.0-vue-chapter03-0-freemarker/src/main/java/io/github/futurewl/BookController.java
+++ spring-boot/spring-boot-2.0-vue/spring-boot-2.0-vue-chapter03/spring-boot-2.0-vue-chapter03-0/spring-boot-2.0-vue-chapter03-0-freemarker/src/main/java/io/github/futurewl/BookController.java
@@ -1,4 +1,4 @@
-package org.sang;
+package io.github.futurewl;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- spring-boot/spring-boot-2.0-vue/spring-boot-2.0-vue-chapter03/spring-boot-2.0-vue-chapter03-0/spring-boot-2.0-vue-chapter03-0-freemarker/src/main/java/io/github/futurewl/SpringBoot20VueChapter3ApplicationFreemarker.java
+++ spring-boot/spring-boot-2.0-vue/spring-boot-2.0-vue-chapter03/spring-boot-2.0-vue-chapter03-0/spring-boot-2.0-vue-chapter03-0-freemarker/src/main/java/io/github/futurewl/SpringBoot20VueChapter3ApplicationFreemarker.java
@@ -1,12 +1,12 @@
-package org.sang;
+package io.github.futurewl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FreemarkerApplication {
+public class SpringBoot20VueChapter3ApplicationFreemarker {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FreemarkerApplication.class, args);
+		SpringApplication.run(SpringBoot20VueChapter3ApplicationFreemarker.class, args);
 	}
 }
