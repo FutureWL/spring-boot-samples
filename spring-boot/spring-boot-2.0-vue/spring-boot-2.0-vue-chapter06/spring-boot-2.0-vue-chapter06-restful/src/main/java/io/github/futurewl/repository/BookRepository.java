b3101962fc17199d48d18ebdbd8f39e231f655de
FutureWL <624263934@qq.com>
2019-08-14 15:58:52 +0800

    更新：

--- spring-boot/spring-boot-2.0-vue/spring-boot-2.0-vue-chapter06/spring-boot-2.0-vue-chapter06-restful/src/main/java/io/github/futurewl/repository/BookRepository.java
+++ spring-boot/spring-boot-2.0-vue/spring-boot-2.0-vue-chapter06/spring-boot-2.0-vue-chapter06-restful/src/main/java/io/github/futurewl/repository/BookRepository.java
@@ -4,12 +4,14 @@ import io.github.futurewl.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * 功能描述：
  *
  * @author weilai
  */
+@CrossOrigin // 开启跨域
 @RepositoryRestResource(
 //        exported = false, // 移除所有增删改查方法
         path = "bs",
