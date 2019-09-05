813bb25e2aac21275da3e021016910aa7f8a3861
FutureWL <624263934@qq.com>
2019-09-05 13:57:03 +0800

    更新：Netty 的单元测试

---
+++ netty/src/main/java/io/github/futurewl/codec/FrameChunkDecoder.java
@@ -0,0 +1,29 @@
+package io.github.futurewl.codec;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.TooLongFrameException;
+
+import java.util.List;
+
+public class FrameChunkDecoder extends ByteToMessageDecoder {
+
+
+    public FrameChunkDecoder(int maxFrameSize) {
+        this.maxFrameSize = maxFrameSize;
+    }
+
+    private final int maxFrameSize;
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        int readableBytes = in.readableBytes();
+        if (readableBytes > maxFrameSize) {
+            in.clear();
+            throw new TooLongFrameException();
+        }
+        ByteBuf buf = in.readBytes(readableBytes);
+        out.add(buf);
+    }
+}
