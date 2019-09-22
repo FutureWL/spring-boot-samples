df10cbb2e7d39a49a12702053b716ba58d97163b
FutureWL <624263934@qq.com>
2019-09-22 09:58:18 +0800

    更新

---
+++ spring-boot/spring-boot-application-security/spring-boot-application-security-oauth2/src/main/java/io/github/futurewl/SpringBootApplicationSecurityOauth2.java
@@ -0,0 +1,11 @@
+package io.github.futurewl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationSecurityOauth2 {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationSecurityOauth2.class, args);
+    }
+}
