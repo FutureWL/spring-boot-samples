75c71c432301a30f5400e8c5243bb20c049002ea
FutureWL <624263934@qq.com>
2019-09-23 23:20:37 +0800

    更新:Java EE

---
+++ java-ee/java-ee-tomcat/src/main/java/io/github/futurewl/TicketServlet.java
@@ -0,0 +1,8 @@
+package io.github.futurewl;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "tickerServlet")
+public class TicketServlet extends HttpServlet {
+}
