2dc41356f04b1e9731bf1b9f9964d0e5701d716c
FutureWL <624263934@qq.com>
2019-09-26 11:27:01 +0800

    更新: 创建 JSP 模块

---
+++ java-ee/java-ee-tomcat/java-ee-tomcat-servlet/src/main/java/io/github/futurewl/Ticket.java
@@ -0,0 +1,35 @@
+package io.github.futurewl;
+
+public class Ticket {
+    private String customerName;
+    private String subject;
+    private String body;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void addAttachment(Attachment attachment) {
+
+    }
+}
--- java-ee/java-ee-tomcat/java-ee-tomcat-servlet/src/main/java/io/github/futurewl/TicketServlet.java
+++ java-ee/java-ee-tomcat/java-ee-tomcat-servlet/src/main/java/io/github/futurewl/TicketServlet.java
@@ -1,8 +1,76 @@
 package io.github.futurewl;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-@WebServlet(name = "tickerServlet")
+@WebServlet(
+        name = "tickerServlet",
+        urlPatterns = {"/tickeets"},
+        loadOnStartup = 1)
+@MultipartConfig(
+        fileSizeThreshold = 5_242_880,
+        maxFileSize = 20_971_520L,
+        maxRequestSize = 41_943_040L)
 public class TicketServlet extends HttpServlet {
+
+    private volatile int TICKET_ID_SEQUENCE = 1;
+
+    private Map<Integer, Ticket> ticketDatabase = new LinkedHashMap<>();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    private void createTicket(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        Ticket ticket = new Ticket();
+        ticket.setCustomerName(request.getParameter("customerName"));
+        ticket.setSubject(request.getParameter("subject"));
+        ticket.setBody(request.getParameter("body"));
+
+        Part filePart = request.getPart("file1");
+        if (filePart != null) {
+            Attachment attachment = this.processAttachment(filePart);
+            if (attachment != null) {
+                ticket.addAttachment(attachment);
+            }
+            int id;
+            synchronized (this) {
+                id = this.TICKET_ID_SEQUENCE++;
+                this.ticketDatabase.put(id, ticket);
+            }
+            response.sendRedirect("tickets?action=view&ticketId=" + id);
+        }
+    }
+
+    private Attachment processAttachment(Part filePart) throws IOException {
+        InputStream inputStream = filePart.getInputStream();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        int read;
+        final byte[] bytes = new byte[1024];
+        while ((read = inputStream.read(bytes)) != -1) {
+            outputStream.write(bytes, 0, read);
+        }
+        Attachment attachment = new Attachment();
+        attachment.setName(filePart.getSubmittedFileName());
+        attachment.setContents(outputStream.toByteArray());
+        return attachment;
+    }
+
+
 }
