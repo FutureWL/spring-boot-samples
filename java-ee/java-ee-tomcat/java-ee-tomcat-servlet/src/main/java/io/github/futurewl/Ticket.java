55aef20ec944db2352f5ccfebf0497bddb5c7c1c
FutureWL <624263934@qq.com>
2019-09-26 11:48:08 +0800

    更新: Servlet 上传文件

--- java-ee/java-ee-tomcat/java-ee-tomcat-servlet/src/main/java/io/github/futurewl/Ticket.java
+++ java-ee/java-ee-tomcat/java-ee-tomcat-servlet/src/main/java/io/github/futurewl/Ticket.java
@@ -1,35 +1,65 @@
 package io.github.futurewl;
 
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Ticket {
     private String customerName;
+
     private String subject;
+
     private String body;
 
-    public String getCustomerName() {
+    private Map<String, Attachment> attachments = new LinkedHashMap<>();
+
+    public String getCustomerName()
+    {
         return customerName;
     }
 
-    public void setCustomerName(String customerName) {
+    public void setCustomerName(String customerName)
+    {
         this.customerName = customerName;
     }
 
-    public String getSubject() {
+    public String getSubject()
+    {
         return subject;
     }
 
-    public void setSubject(String subject) {
+    public void setSubject(String subject)
+    {
         this.subject = subject;
     }
 
-    public String getBody() {
+    public String getBody()
+    {
         return body;
     }
 
-    public void setBody(String body) {
+    public void setBody(String body)
+    {
         this.body = body;
     }
 
-    public void addAttachment(Attachment attachment) {
+    public Attachment getAttachment(String name)
+    {
+        return this.attachments.get(name);
+    }
 
+    public Collection<Attachment> getAttachments()
+    {
+        return this.attachments.values();
+    }
+
+    public void addAttachment(Attachment attachment)
+    {
+        this.attachments.put(attachment.getName(), attachment);
+    }
+
+    public int getNumberOfAttachments()
+    {
+        return this.attachments.size();
     }
 }
