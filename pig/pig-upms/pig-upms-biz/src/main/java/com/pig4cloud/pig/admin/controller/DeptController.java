fda751f2824e3abb340b966079290223c00532da
FutureWL <624263934@qq.com>
2019-09-28 22:52:37 +0800

    更新:

---
+++ pig/pig-upms/pig-upms-biz/src/main/java/com/pig4cloud/pig/admin/controller/DeptController.java
@@ -0,0 +1,114 @@
+/*
+ *  Copyright (c) 2019-2020, 冷冷 (wangiegie@gmail.com).
+ *  <p>
+ *  Licensed under the GNU Lesser General Public License 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ * https://www.gnu.org/licenses/lgpl.html
+ *  <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pig4cloud.pig.admin.controller;
+
+import com.pig4cloud.pig.admin.api.entity.SysDept;
+import com.pig4cloud.pig.admin.service.SysDeptService;
+import com.pig4cloud.pig.common.core.util.R;
+import com.pig4cloud.pig.common.log.annotation.SysLog;
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 部门管理 前端控制器
+ * </p>
+ *
+ * @author lengleng
+ * @since 2019/2/1
+ */
+@RestController
+@AllArgsConstructor
+@RequestMapping("/dept")
+public class DeptController {
+	private final SysDeptService sysDeptService;
+
+	/**
+	 * 通过ID查询
+	 *
+	 * @param id ID
+	 * @return SysDept
+	 */
+	@GetMapping("/{id}")
+	public R getById(@PathVariable Integer id) {
+		return R.ok(sysDeptService.getById(id));
+	}
+
+
+	/**
+	 * 返回树形菜单集合
+	 *
+	 * @return 树形菜单
+	 */
+	@GetMapping(value = "/tree")
+	public R listDeptTrees() {
+		return R.ok(sysDeptService.listDeptTrees());
+	}
+
+	/**
+	 * 返回当前用户树形菜单集合
+	 *
+	 * @return 树形菜单
+	 */
+	@GetMapping(value = "/user-tree")
+	public R listCurrentUserDeptTrees() {
+		return R.ok(sysDeptService.listCurrentUserDeptTrees());
+	}
+
+	/**
+	 * 添加
+	 *
+	 * @param sysDept 实体
+	 * @return success/false
+	 */
+	@SysLog("添加部门")
+	@PostMapping
+	@PreAuthorize("@pms.hasPermission('sys_dept_add')")
+	public R save(@Valid @RequestBody SysDept sysDept) {
+		return R.ok(sysDeptService.saveDept(sysDept));
+	}
+
+	/**
+	 * 删除
+	 *
+	 * @param id ID
+	 * @return success/false
+	 */
+	@SysLog("删除部门")
+	@DeleteMapping("/{id}")
+	@PreAuthorize("@pms.hasPermission('sys_dept_del')")
+	public R removeById(@PathVariable Integer id) {
+		return R.ok(sysDeptService.removeDeptById(id));
+	}
+
+	/**
+	 * 编辑
+	 *
+	 * @param sysDept 实体
+	 * @return success/false
+	 */
+	@SysLog("编辑部门")
+	@PutMapping
+	@PreAuthorize("@pms.hasPermission('sys_dept_edit')")
+	public R update(@Valid @RequestBody SysDept sysDept) {
+		sysDept.setUpdateTime(LocalDateTime.now());
+		return R.ok(sysDeptService.updateDeptById(sysDept));
+	}
+}
