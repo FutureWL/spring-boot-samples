297f17695564789313290b300bfa081e2eb64307
FutureWL <624263934@qq.com>
2019-09-28 21:13:51 +0800

    更新: activiti spring boot

---
+++ activiti/activiti-crazy/activiti-crazy-1604-activiti6-spring-boot/src/main/java/io/github/futurewl/ActivitiCrazy1604Activiti6SpringBoot.java
@@ -0,0 +1,11 @@
+package io.github.futurewl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiCrazy1604Activiti6SpringBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiCrazy1604Activiti6SpringBoot.class, args);
+    }
+}
