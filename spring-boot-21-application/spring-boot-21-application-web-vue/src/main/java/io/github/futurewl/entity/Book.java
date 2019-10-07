a200cb2a3a76b8a06ecb4aa1531b7763bf7783bc
FutureWL <624263934@qq.com>
2019-10-07 20:10:27 +0800

    更新：整理项目

---
+++ spring-boot-21-application/spring-boot-21-application-web-vue/src/main/java/io/github/futurewl/entity/Book.java
@@ -0,0 +1,45 @@
+package io.github.futurewl.entity;
+
+public class Book {
+
+    private String id;
+
+    private String name;
+
+    private String author;
+
+    public Book() {
+        super();
+    }
+
+    public Book(String id, String name, String author) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
