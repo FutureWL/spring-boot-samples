46ae0a02015e692f3d6a29b81f0e99cd5091462c
Future. 魏来 <624263934@qq.com>
2022-01-07 17:53:16 +0800

    更新：完成官方文档的案例 web

---
+++ web/README.md
@@ -0,0 +1,10 @@
+# Web 
+
+> Servlet Web, Reactive Web, Embedded Container Support, Graceful Shutdown, and more.
+
+1. Servlet Web Application
+2. Reactive Web Application
+3. Graceful Shutdown 平滑关闭
+4. Spring Security
+5. Spring Session
+6. Spring HATEOAS
---
+++ web/src/main/java/io/github/futurewl/sbs/v26x/WebV26xApp.java
@@ -0,0 +1,11 @@
+package io.github.futurewl.sbs.v26x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebV26xApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WebV26xApp.class, args);
+    }
+}
