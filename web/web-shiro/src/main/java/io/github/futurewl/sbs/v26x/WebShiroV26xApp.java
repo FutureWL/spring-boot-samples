cc2a2c01dbd15af962b9abd8e09d7e6cef5ae0f1
Future. 魏来 <624263934@qq.com>
2022-03-06 22:22:52 +0800

    更新: web-shiro

---
+++ web/web-shiro/src/main/java/io/github/futurewl/sbs/v26x/WebShiroV26xApp.java
@@ -0,0 +1,11 @@
+package io.github.futurewl.sbs.v26x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShiroV26xApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WebShiroV26xApp.class, args);
+    }
+}
